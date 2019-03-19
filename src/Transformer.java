e8b7fe8dd58313b08bbf2ec70afb92f1bd487f73
Steve Coopman (CIS) <ST20018615@wipro.com>
2019-03-19 15:07:10 -0500

    revert changes to try to lambdafy

--- src/Transformer.java
+++ src/Transformer.java
@@ -29,42 +29,41 @@ public class Transformer {
 	private static void writeAll(FileWriter o) throws IOException {
 		
 		//preblock
-		o.write("#SingleInstance force\n");
 		o.write("coordmode, mouse, screen\n");
 		o.write("J::\n");
 		//endpreblock
 		
 		//repeated block
-		int x;
-		int y;
+		int xTemp;
+		int yTemp;
 		for(int i = 0;i<5;i++) {
 			for( int j = 0; j < 2; j++) {
-				x = xOrigin + (xOffset * i);
-				y = yOrigin + (yOffset * j);
+				xTemp = xOrigin + (xOffset * i);
+				yTemp = yOrigin + (yOffset * j);
 							
 				o.write( //concat series of method calls or final strings
-					mm(x, y)+
-					//click+ 
-					sleep(10)			
+					buildMouseMove(xTemp, yTemp)+
+					click+ 
+					buildSleep(10)			
 				);	
 			}	
 		}
-		o.write(sleep(10));
+		o.write(buildSleep(10));
 		//endrepeatedblock
 		
 		//post block
 		o.write("return\n");
 		
-		o.write("Escape::\n" + mm(50, 50) + "ExitApp\nReturn\n");
+		o.write("Escape::\n" + buildMouseMove(50, 50) + "ExitApp\nReturn\n");
 		//end post block		
 	}
 
 
-	public static String mm(int x, int y) {
+	public static String buildMouseMove(int x, int y) {
 		return "MouseMove, " + x + ", " + y + "\n";
 	}
 	
-	public static String sleep(int ms) {
+	public static String buildSleep(int ms) {
 		return "sleep, " + ms + "\n";
 	}
 
